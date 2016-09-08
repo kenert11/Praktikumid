59eab4f0f670c37590b047413f686f64830915d1
Kenert Joost <kjoost@klass1-29-l.intra.itcollege.ee>
2016-09-08 14:26:12 +0300

    Praktikumid

---
+++ Praktikumid/src/praktikum1/Tereteine.java
@@ -0,0 +1,14 @@
+package praktikum1;
+
+public class Tereteine {
+public static void main (String[] kasurida) {
+			System.out.println(
+			"*****  *****  ****   *****  *\n"+
+			"  *    *      *   *  *      *\n"+
+			"  *    *****  ****   *****  *\n"+
+			"  *    *      * *    *       \n"+
+			"  *    *****  *  *   *****  *\n"
+					);
+}
+}
+//Ctrl shift +f Format

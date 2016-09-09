9880eb1b03e2eb7e7125312cd1dded42c2abd696
Kenert Joost <kjoost@klass1-27-l.intra.itcollege.ee>
2016-09-09 13:08:27 +0300

    Praktikum4

--- Praktikumid/src/praktikum4/tehisintellekt.java
+++ Praktikumid/src/praktikum4/tehisintellekt.java
@@ -3,16 +3,23 @@ package praktikum4;
 import lib.TextIO;
 
 public class tehisintellekt {
-	public static void main(String [] args){
+	public static void main(String[] args) {
 		System.out.println("Palun sisesta kaks vanust");
 		int vanus1 = TextIO.getInt();
+		if (vanus1 < 0) {
+			System.out.println("Proovi andmed uuesti sisestada");
+			return;
+		}
 		int vanus2 = TextIO.getInt();
-		
+		if (vanus2 < 0) {
+			System.out.println("Proovi andmed uuesti sisestada");
+			return;
+		}
 		int vanuseVahe = Math.abs(vanus1 - vanus2);
-		if(vanuseVahe > 5){
-			
+		if (vanuseVahe > 5) {
+
 			System.out.println("Üpriski palju");
-		} else if (vanuseVahe> 10){
+		} else if (vanuseVahe > 10) {
 			System.out.println("See on ikka väga suur vahe");
 		} else {
 			System.out.println("Sobib");

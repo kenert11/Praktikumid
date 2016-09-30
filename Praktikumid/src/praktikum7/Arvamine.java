afb97460f2913f3c8196de21031c20e45593eb36
Kenert Joost <kjoost@klass1-11-l.intra.itcollege.ee>
2016-09-30 12:28:12 +0300

    Praktikum7

---
+++ Praktikumid/src/praktikum7/Arvamine.java
@@ -0,0 +1,28 @@
+package praktikum7;
+import java.util.Random;
+
+import lib.TextIO;
+public class Arvamine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random= new Random();
+		int number= random.nextInt(100);
+		int arvamus= 0;
+		int arvamusteArv = 0;
+		while(number !=arvamus){
+			System.out.println("Sisesta arv vahemikus 1-100:");
+			arvamus=TextIO.getInt();
+			arvamusteArv++;
+			if(arvamus<number){
+				System.out.println("Liiga väike arv, proovi uuesti");
+			}else if(arvamus>number){
+				System.out.println("Liiga kõrge arv, proovi uuesti");
+			}else{
+				System.out.println("Arvasid õigesti"+ " " + number + " " + "proovisid" + " " + arvamusteArv + " " + "korda" );
+			}
+		}
+		
+	}
+
+}
